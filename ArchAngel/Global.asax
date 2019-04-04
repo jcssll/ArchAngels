7338e3dd5d16e81190d011f8b0dbca3652a52c9d
jcssll <essentialnetworker@gmail.com>
2019-04-04 12:20:38 -0400

    Archangels MVC SetUp
    Copied folders over

---
+++ ArchAngel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArchAngel.MvcApplication" Language="C#" %>
---
+++ ArchAngel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArchAngel.Startup))]
+namespace ArchAngel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
